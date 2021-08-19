e79826f491fac7dd0b4f6d6d5e2dfab775864e6e
Moy <moymtz2050@gmail.com>
2021-08-19 16:16:57 -0500

    Cambios en Index.php

--- index.php
+++ index.php
@@ -12,7 +12,9 @@ if(!isset($_SESSION['user'])){
 <body>
 
 <h1>Hi, this is my index</h1>
-<h2>This is my first change</h2>
+<h2>This Is my first change</h2>
+This is my thirst change
+and this is my fourth change
 
 Welcome <?php echo $_SESSION['user'];?><br><br>
 <a href="cerrarSesion.php">Cerrar sesión</a>
